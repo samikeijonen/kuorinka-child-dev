e8b23d8dd5d2273befbe73f75b2377daf9083859
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-08-29 17:21:12 +0300

    Add all files.

---
+++ functions.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * This is child themes functions.php file. All modifications should be made in this file.
+ *
+ * All style changes should be in child themes style.css file.
+ *
+ * @package    Kuorinka Child Dev
+ * @author     Sami Keijonen <sami.keijonen@foxnet.fi>
+ * @copyright  Copyright (c) 2014, Sami Keijonen
+ * @link       https://foxnet-themes.fi/downloads/kuorinka
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/* Adds the child theme setup function to the 'after_setup_theme' hook. */
+add_action( 'after_setup_theme', 'kuorinka_child_dev_theme_setup', 11 );
+
+/**
+ * Setup function.  All child themes should run their setup within this function.  The idea is to add/remove 
+ * filters and actions after the parent theme has been set up.  This function provides you that opportunity.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function kuorinka_child_dev_theme_setup() {
+
+	/* Load child theme text domain. */
+	load_child_theme_textdomain( 'kuorinka-child-dev', get_stylesheet_directory() . '/languages' );
+	
+	/*
+	 * Add a custom background to overwrite the defaults. Remove this section if you want to use 
+	 * the parent theme defaults instead.
+	 *
+	 * @link http://codex.wordpress.org/Custom_Backgrounds
+	 */
+	add_theme_support(
+		'custom-background',
+		array(
+			'default-color' => 'f7f7f7',
+			'default-image' => ''
+		)
+	);
+	
+	/*
+	 * Add a custom header to overwrite the defaults. Remove this section if you want to use the 
+	 * the parent theme defaults instead.
+	 *
+	 * @link http://codex.wordpress.org/Custom_Headers
+	 */
+	add_theme_support(
+		'custom-header',
+		array(
+			'default-text-color' => '2e2e2e',
+			'default-image'      => get_stylesheet_directory_uri() . '/images/headers/child.jpg'
+		)
+	);
+	
+	/*
+	 * Registers default headers for the theme. If you don't want to add custom headers, remove 
+	 * this section. Note: Header thumbnail sizes should be 230x78.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/register_default_headers
+	 */
+	register_default_headers( array(
+		'child' => array(
+			'url'           => '%2$s/images/headers/child.jpg',
+			'thumbnail_url' => '%2$s/images/headers/child-thumbnail.jpg',
+			'description'   => __( 'Test header', 'kuorinka-child-dev' )
+		)
+	) );
+	
+	/* Add child theme fonts to editor styles. */
+	add_editor_style( kuorinka_child_dev_fonts_url() );
+	
+}
+
+/**
+ * Enqueue scripts and styles.
+ *
+ * @since  1.0.0
+ */
+function kuorinka_child_dev_scripts() {
+	
+	/* Dequeue parent fonts. */
+	wp_dequeue_style( 'kuorinka-fonts' );
+	
+	/* Enqueue fonts. */
+	wp_enqueue_style( 'kuorinka-child-dev-fonts', kuorinka_child_dev_fonts_url(), array(), null );
+	
+}
+add_action( 'wp_enqueue_scripts', 'kuorinka_child_dev_scripts', 11 );
+
+/**
+ * Enqueue theme fonts in admin header page.
+ *
+ * @since  1.0.0
+ */
+function kuorinka_child_dev_custom_header_fonts() {
+	wp_enqueue_style( 'kuorinka-child-dev-fonts', kuorinka_child_dev_fonts_url(), array(), null );
+}
+add_action( 'admin_print_styles-appearance_page_custom-header', 'kuorinka_child_dev_custom_header_fonts' );
+
+/**
+ * Return the Google font stylesheet URL
+ *
+ * @since  1.0.0
+ * @return string
+ */
+function kuorinka_child_dev_fonts_url() {
+
+	$fonts_url = '';
+
+	/* Translators: If there are characters in your language that are not
+	 * supported by Lato, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$lato = _x( 'on', 'Lato font: on or off', 'kuorinka-child-dev' );
+
+	/* Translators: If there are characters in your language that are not
+	 * supported by Arimo, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$playfair_display = _x( 'on', 'Arimo font: on or off', 'kuorinka-child-dev' );
+
+	if ( 'off' !== $lato || 'off' !== $playfair_display ) {
+		$font_families = array();
+
+		if ( 'off' !== $lato )
+			$font_families[] = 'Lato:300,400,700,900,300italic,400italic,700italic,900italic';
+
+		if ( 'off' !== $playfair_display )
+			$font_families[] = 'Arimo:400,700,400italic,700italic';
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+		$fonts_url = add_query_arg( $query_args, "//fonts.googleapis.com/css" );
+	}
+
+	return $fonts_url;
+}
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Kuorinka Child Dev
+
+Short description of your theme. This is development and example child theme for Kuorinka parent theme. 
+
+## Understanding Kuorinka
+
+The Kuorinka parent theme is combination of [_s](https://github.com/Automattic/_s) theme, some of my own experience and some files and ideas from
+[Hybrid Core](http://themehybrid.com/hybrid-core). Note that theme doesn't use Hybrid Core, only some files from it.
+
+Kuorinka theme is accessibility-ready and translation-ready, have right to left support and includes [Schema.org](http://schema.org) microdata.
+I expect that child theme do not break those things. For example check color contrast with [Joe Dolson tool](https://www.joedolson.com/tools/color-contrast.php)
+so you can keep accessibility-ready tag.
+
+## Translations
+
+All child themes should have a `/languages/theme-slug.pot` file. Even if your theme doesn't add new strings, the `style.css` headers should be translatable.
+
+If you're unfamiliar with this process, here are two tutorials for creating a POT file:
+
+* [Creating pot files](https://foxnet-themes.fi/creating-pot-file-for-your-theme-or-plugin/)
+* [Language packs](http://ottopress.com/2013/language-packs-101-prepwork)
+* [Create pot file using Grunt task](https://www.npmjs.org/package/grunt-wp-i18n)
+
+## Copyright and License
+
+The following resources are not included with the theme but are external resources linked to within the theme.
+
+* [Lato](https://www.google.com/fonts/specimen/Lato) by Łukasz Dziedzic - Licensed under the [SIL Open Font License, version 1.1](http://scripts.sil.org/OFL).
+* [Arimo](http://www.google.com/fonts/specimen/Arimo) by Steve Matteson - Licensed under the [Apache License, version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html).
+
+All other resources and theme elements are licensed under the [GNU GPL](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html), version 2 or later.
+
+2014 &copy; [Sami Keijonen](https://foxnet-themes.fi).
+
+## Changelog
+
+### Version 1.0.0
+
+* Everything's new!
